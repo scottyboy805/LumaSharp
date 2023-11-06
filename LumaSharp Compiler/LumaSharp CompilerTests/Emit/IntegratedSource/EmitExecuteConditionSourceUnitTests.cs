4b02fc7fd001ec7fdad04d752a8d4867fbb45039
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2023-11-06 20:38:14 +0000

    Add tests for compiling and executing conditions with alternate branches

--- LumaSharp Compiler/LumaSharp CompilerTests/Emit/IntegratedSource/EmitExecuteConditionSourceUnitTests.cs
+++ LumaSharp Compiler/LumaSharp CompilerTests/Emit/IntegratedSource/EmitExecuteConditionSourceUnitTests.cs
@@ -84,7 +84,7 @@ namespace LumaSharp_CompilerTests.Emit.IntegratedSource
         }
 
         [TestMethod]
-        public void EmitExecuteCondition_SimpleAlternate_True()
+        public void EmitExecuteCondition_SimpleElse_True()
         {
             SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
             type Test
@@ -120,7 +120,7 @@ namespace LumaSharp_CompilerTests.Emit.IntegratedSource
         }
 
         [TestMethod]
-        public void EmitExecuteCondition_SimpleAlternate_False()
+        public void EmitExecuteCondition_SimpleElse_False()
         {
             SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
             type Test
@@ -131,6 +131,154 @@ namespace LumaSharp_CompilerTests.Emit.IntegratedSource
                         return 33;
                     else
                         return 22;
+                }
+            }"));
+
+            // Create model
+            SemanticModel model = SemanticModel.BuildModel("Test", new SyntaxTree[] { tree }, null);
+            ConditionModel conditionModel = model.DescendantsOfType<ConditionModel>(true).FirstOrDefault();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(conditionModel);
+            Assert.AreEqual(0, model.Report.MessageCount);
+
+            // Build instructions
+            MemoryStream stream = new MemoryStream();
+            InstructionBuilder builder = new InstructionBuilder(new BinaryWriter(stream));
+            new MethodBodyBuilder(new StatementModel[] { conditionModel }).BuildEmitObject(builder);
+
+            // Execute code
+            __memory.InitStack();
+            __interpreter.ExecuteBytecode(stream.ToArray());
+
+            // Get value on top of stack
+            Assert.AreEqual(22, __interpreter.FetchValue<int>());
+        }
+
+        [TestMethod]
+        public void EmitExecuteCondition_SimpleAlternate_True()
+        {
+            SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
+            type Test
+            {
+                i32 Method()
+                {
+                    if(true) 
+                        return 33;
+                    else if(false)
+                        return 22;
+                }
+            }"));
+
+            // Create model
+            SemanticModel model = SemanticModel.BuildModel("Test", new SyntaxTree[] { tree }, null);
+            ConditionModel conditionModel = model.DescendantsOfType<ConditionModel>(true).FirstOrDefault();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(conditionModel);
+            Assert.AreEqual(0, model.Report.MessageCount);
+
+            // Build instructions
+            MemoryStream stream = new MemoryStream();
+            InstructionBuilder builder = new InstructionBuilder(new BinaryWriter(stream));
+            new MethodBodyBuilder(new StatementModel[] { conditionModel }).BuildEmitObject(builder);
+
+            // Execute code
+            __memory.InitStack();
+            __interpreter.ExecuteBytecode(stream.ToArray());
+
+            // Get value on top of stack
+            Assert.AreEqual(33, __interpreter.FetchValue<int>());
+        }
+
+        [TestMethod]
+        public void EmitExecuteCondition_SimpleAlternate_False()
+        {
+            SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
+            type Test
+            {
+                i32 Method()
+                {
+                    if(false) 
+                        return 33;
+                    else if(true)
+                        return 22;
+                }
+            }"));
+
+            // Create model
+            SemanticModel model = SemanticModel.BuildModel("Test", new SyntaxTree[] { tree }, null);
+            ConditionModel conditionModel = model.DescendantsOfType<ConditionModel>(true).FirstOrDefault();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(conditionModel);
+            Assert.AreEqual(0, model.Report.MessageCount);
+
+            // Build instructions
+            MemoryStream stream = new MemoryStream();
+            InstructionBuilder builder = new InstructionBuilder(new BinaryWriter(stream));
+            new MethodBodyBuilder(new StatementModel[] { conditionModel }).BuildEmitObject(builder);
+
+            // Execute code
+            __memory.InitStack();
+            __interpreter.ExecuteBytecode(stream.ToArray());
+
+            // Get value on top of stack
+            Assert.AreEqual(22, __interpreter.FetchValue<int>());
+        }
+
+        [TestMethod]
+        public void EmitExecuteCondition_SimpleAlternateElse_1()
+        {
+            SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
+            type Test
+            {
+                i32 Method()
+                {
+                    if(1) 
+                        return 33;
+                    else if(2)
+                        return 22;
+                    else
+                        return 11;
+                }
+            }"));
+
+            // Create model
+            SemanticModel model = SemanticModel.BuildModel("Test", new SyntaxTree[] { tree }, null);
+            ConditionModel conditionModel = model.DescendantsOfType<ConditionModel>(true).FirstOrDefault();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(conditionModel);
+            Assert.AreEqual(0, model.Report.MessageCount);
+
+            // Build instructions
+            MemoryStream stream = new MemoryStream();
+            InstructionBuilder builder = new InstructionBuilder(new BinaryWriter(stream));
+            new MethodBodyBuilder(new StatementModel[] { conditionModel }).BuildEmitObject(builder);
+
+            // Execute code
+            __memory.InitStack();
+            __interpreter.ExecuteBytecode(stream.ToArray());
+
+            // Get value on top of stack
+            Assert.AreEqual(33, __interpreter.FetchValue<int>());
+        }
+
+        [TestMethod]
+        public void EmitExecuteCondition_SimpleAlternateElse_2()
+        {
+            SyntaxTree tree = SyntaxTree.Parse(InputSource.FromSourceText(@"
+            type Test
+            {
+                i32 Method()
+                {
+                    if(2) 
+                        return 33;
+                    else if(1)
+                        return 22;
+                    else
+                        return 11;
                 }
             }"));
 
